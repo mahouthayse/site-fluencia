b07f73bda095b9305cfdc9b8365c67c5886af8ac
mahouthayse <dasilva.thaysef@gmail.com>
2020-03-25 18:40:32 -0300

    Cupon de desconto

---
+++ build/precache-manifest.3cc312317dab25129a882456748b005d.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e1c12c07c0e5bfc26e60c3ab0db87e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e6ecd9598e86bd7630e",
+    "url": "/static/css/2.ea8e17d9.chunk.css"
+  },
+  {
+    "revision": "6f80eb17a24c379ba051",
+    "url": "/static/css/main.2b1b31ca.chunk.css"
+  },
+  {
+    "revision": "8e6ecd9598e86bd7630e",
+    "url": "/static/js/2.aec82c39.chunk.js"
+  },
+  {
+    "revision": "ba5eb6aac2299e49bd603a5de538d272",
+    "url": "/static/js/2.aec82c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f80eb17a24c379ba051",
+    "url": "/static/js/main.c2eaf5bd.chunk.js"
+  },
+  {
+    "revision": "835086cd6be21d629bf1",
+    "url": "/static/js/runtime-main.bc373420.js"
+  },
+  {
+    "revision": "878b9f2a2e04b0bd1fef9b0e082072a0",
+    "url": "/static/media/apostila.878b9f2a.pdf"
+  },
+  {
+    "revision": "0ef66fc8486d058853f42bb1fb477c80",
+    "url": "/static/media/auto_avaliação.0ef66fc8.pdf"
+  },
+  {
+    "revision": "809d113bbd278156752354ffa4ddcd0e",
+    "url": "/static/media/benefitsImage.809d113b.png"
+  },
+  {
+    "revision": "5083c493f6d2eb9a66cd012213d67bb5",
+    "url": "/static/media/carol.5083c493.png"
+  },
+  {
+    "revision": "38e6d422ae4e10cfdef819a2dbe3b6ed",
+    "url": "/static/media/certificado1.38e6d422.png"
+  },
+  {
+    "revision": "c70ee3c95c1e19d767f7d3e880fbff74",
+    "url": "/static/media/certificado2.c70ee3c9.png"
+  },
+  {
+    "revision": "ad16cb0167a98b6e13e7e8f42f8288a6",
+    "url": "/static/media/checkoutBackground.ad16cb01.jpg"
+  },
+  {
+    "revision": "20d98cde0ac9dd71696ad8bc1fa4339d",
+    "url": "/static/media/download.20d98cde.svg"
+  },
+  {
+    "revision": "be6a5c1c4ac07e695c5742d2299208c0",
+    "url": "/static/media/flue.be6a5c1c.png"
+  },
+  {
+    "revision": "188d7160b7fca9c0572f86443be4af16",
+    "url": "/static/media/grafico_altas.188d7160.pdf"
+  },
+  {
+    "revision": "9689c65f053f84e00337ba3cf9d0bef2",
+    "url": "/static/media/grafico_baixas.9689c65f.pdf"
+  },
+  {
+    "revision": "090d7fd7487931e64b3428c8d5ba748a",
+    "url": "/static/media/group43.090d7fd7.svg"
+  },
+  {
+    "revision": "c1f97f8e8dcd0e95ec681367660be9dd",
+    "url": "/static/media/group98.c1f97f8e.svg"
+  },
+  {
+    "revision": "248d207e94b425a3ac62c9f889e4bdb4",
+    "url": "/static/media/guia_rápido.248d207e.pdf"
+  },
+  {
+    "revision": "2c778ed0f6d3ca0cc0a5e3f34cd6408f",
+    "url": "/static/media/header.2c778ed0.jpg"
+  },
+  {
+    "revision": "e5861bce43d5c6b827377ab5d4865ee9",
+    "url": "/static/media/headerCurso.e5861bce.png"
+  },
+  {
+    "revision": "056325464d6aaa8e6ddbd5fc0360c4ff",
+    "url": "/static/media/headerGroup.05632546.png"
+  },
+  {
+    "revision": "9546b2e85e6debfa53f7499defc4f42f",
+    "url": "/static/media/headerIndividual.9546b2e8.png"
+  },
+  {
+    "revision": "aa4a27fb371e498db4f7ecebedda9903",
+    "url": "/static/media/headerInicial.aa4a27fb.png"
+  },
+  {
+    "revision": "d5cc87a28283ac623811b62a0212ed06",
+    "url": "/static/media/headerTriangle.d5cc87a2.svg"
+  },
+  {
+    "revision": "d2304f0b52f966a7f66cf5f43eb93018",
+    "url": "/static/media/img1.d2304f0b.png"
+  },
+  {
+    "revision": "6e2506a492f3c5ce771f1d5b6135de40",
+    "url": "/static/media/img2.6e2506a4.png"
+  },
+  {
+    "revision": "69e0acac5bf3e3afcf04262cf3901615",
+    "url": "/static/media/img3.69e0acac.png"
+  },
+  {
+    "revision": "8f7949fc5c3f49744b157e49418e52af",
+    "url": "/static/media/img4.8f7949fc.png"
+  },
+  {
+    "revision": "056325464d6aaa8e6ddbd5fc0360c4ff",
+    "url": "/static/media/imgGrupo.05632546.png"
+  },
+  {
+    "revision": "9546b2e85e6debfa53f7499defc4f42f",
+    "url": "/static/media/imgIndividual.9546b2e8.png"
+  },
+  {
+    "revision": "3468bb2baec111621fb2b2818dc1d45e",
+    "url": "/static/media/imgWork.3468bb2b.png"
+  },
+  {
+    "revision": "9338638bfae487a4883d9e345eed3715",
+    "url": "/static/media/infoImage1.9338638b.png"
+  },
+  {
+    "revision": "1df5d105bb54a982a1b864b3fbc9418d",
+    "url": "/static/media/infoImage2.1df5d105.png"
+  },
+  {
+    "revision": "d9ac44b687accfdd74b5e17e57ebd843",
+    "url": "/static/media/infoImage3.d9ac44b6.png"
+  },
+  {
+    "revision": "ca4cf0288a6faf701767a89b7821dd8c",
+    "url": "/static/media/insta1.ca4cf028.jpg"
+  },
+  {
+    "revision": "7cb0414dd2715328521a74efaf05127f",
+    "url": "/static/media/insta2.7cb0414d.jpg"
+  },
+  {
+    "revision": "07c67dc8ebb9e15dff14ebd7211f7ec1",
+    "url": "/static/media/insta3.07c67dc8.png"
+  },
+  {
+    "revision": "0845524ae8937c5c860bfc70269792da",
+    "url": "/static/media/insta4.0845524a.jpg"
+  },
+  {
+    "revision": "ad16cb0167a98b6e13e7e8f42f8288a6",
+    "url": "/static/media/loginBackground.ad16cb01.jpg"
+  },
+  {
+    "revision": "e8bda52c0a881e7862c99561e445e8e0",
+    "url": "/static/media/mail.e8bda52c.png"
+  },
+  {
+    "revision": "5057e93c4ad6dea4c14e33f0c424ba79",
+    "url": "/static/media/materiais.5057e93c.png"
+  },
+  {
+    "revision": "35312a57ee3306c9be9ed16a3512eec5",
+    "url": "/static/media/materialApoio.35312a57.png"
+  },
+  {
+    "revision": "22b3df9842c4d5c857404e56d7fa9c2d",
+    "url": "/static/media/parâmetros_referência.22b3df98.pdf"
+  },
+  {
+    "revision": "ae62a1b7f4aedaf5f881dde3cd007e8b",
+    "url": "/static/media/previk.ae62a1b7.gif"
+  },
+  {
+    "revision": "ad16cb0167a98b6e13e7e8f42f8288a6",
+    "url": "/static/media/pricesImage.ad16cb01.jpg"
+  }
+]);
